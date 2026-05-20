2e71e59327c2b1e69501b85cc441764e8385d447
David Alejandro Luna <david.lunamar@campusucc.edu.co>
2026-05-20 02:26:49 -0500

    error carga de escena ala norte

--- Assets/Scripts/Editor/SceneNarrativeConfigurator.cs
+++ Assets/Scripts/Editor/SceneNarrativeConfigurator.cs
@@ -204,7 +204,7 @@ public class SceneNarrativeConfigurator : EditorWindow
         new DoorConfig
         {
             name = "Door_ToNorthStreet",
-            targetRoomId = "ala_norte",
+            targetRoomId = "northstreet",
             requiredChapterId = "chapter4",
             requiredFlag = "",
             requiredKeys = new KeyType[] { KeyType.SingleUseKey },
@@ -219,7 +219,7 @@ public class SceneNarrativeConfigurator : EditorWindow
         new DoorConfig
         {
             name = "Door_ToEmptyRoom",
-            targetRoomId = "empty_room",
+            targetRoomId = "emptyRoom",
             requiredChapterId = "chapter5",
             requiredFlag = "",
             requiredKeys = new KeyType[] { KeyType.SingleUseKey },
@@ -234,7 +234,7 @@ public class SceneNarrativeConfigurator : EditorWindow
         new DoorConfig
         {
             name = "Door_ToKidnappedSimon",
-            targetRoomId = "simon_captive",
+            targetRoomId = "KidNappedSimon",
             requiredChapterId = "chapter5",
             requiredFlag = "",
             requiredKeys = new KeyType[] { KeyType.SingleUseKey },
@@ -249,7 +249,7 @@ public class SceneNarrativeConfigurator : EditorWindow
         new DoorConfig
         {
             name = "Door_ToKillerBunker",
-            targetRoomId = "killer_bunker",
+            targetRoomId = "killerbunker",
             requiredChapterId = "chapter5",
             requiredFlag = "",
             requiredKeys = new KeyType[] { KeyType.SingleUseKey },
